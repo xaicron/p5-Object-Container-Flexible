426121758b8817702270a7d0bcabd6c8dc699e56
xaicron <xaicron@gmail.com>
2010-10-02 23:33:48 +0900

    initial commit

---
+++ lib/Object/Container/Flexible.pm
@@ -0,0 +1,42 @@
+package Object::Container::Flexible;
+
+use strict;
+use warnings;
+use 5.008_001;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=for stopwords
+
+=head1 NAME
+
+Object::Container::Flexible -
+
+=head1 SYNOPSIS
+
+  use Object::Container::Flexible;
+
+=head1 DESCRIPTION
+
+Object::Container::Flexible is
+
+=head1 AUTHOR
+
+xaicron E<lt>xaicron {at} cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2010 - xaicron
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
